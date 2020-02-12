c40301caaf4093624ffc844cdfba94cbd74653cd
seiji-s <seiji.swingl@gmail.com>
2020-02-11 16:27:44 -0800

    finished product for babylist takehome

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  //initial starting deck (unshuffled)
+  //My linter formats the array in this way so sorry if this is hard to look at.
+  state = {
+    deck: [
+      "A♠",
+      "2♠",
+      "3♠",
+      "4♠",
+      "5♠",
+      "6♠",
+      "7♠",
+      "8♠",
+      "9♠",
+      "10♠",
+      "J♠",
+      "Q♠",
+      "K♠",
+      "A♥",
+      "2♥",
+      "3♥",
+      "4♥",
+      "5♥",
+      "6♥",
+      "7♥",
+      "8♥",
+      "9♥",
+      "10♥",
+      "J♥",
+      "Q♥",
+      "K♥",
+      "A♦",
+      "2♦",
+      "3♦",
+      "4♦",
+      "5♦",
+      "6♦",
+      "7♦",
+      "8♦",
+      "9♦",
+      "10♦",
+      "J♦",
+      "Q♦",
+      "K♦",
+      "A♣",
+      "2♣",
+      "3♣",
+      "4♣",
+      "5♣",
+      "6♣",
+      "7♣",
+      "8♣",
+      "9♣",
+      "10♣",
+      "J♣",
+      "Q♣",
+      "K♣"
+    ],
+    discard: [],
+    currentCard: ""
+  };
+  //function to randomize / shuffle the starting deck
+  //this is only here to prevent the hassle of having to shuffle the deck multiple times
+  shuffleArray(array) {
+    let holder = [...array];
+    for (let i = holder.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [holder[i], holder[j]] = [holder[j], holder[i]];
+    }
+    this.setState({ deck: holder });
+  }
+
+  //function to imitate hand shuffling
+  handShuffle(array) {
+    //splits deck in half
+    let array2 = [...array];
+    array2 = array.splice(0, Math.ceil(array.length / 2));
+    let array3 = [];
+    //interleaves cards
+    for (let i = 0; i < array.length; i++) {
+      array3.push(array[i], array2[i]);
+    }
+    //set back to original deck
+    this.setState({ deck: array3 });
+  }
+
+  //picks the top card from the deck
+  //reshuffles deck when empty
+
+  pickACard(d1, d2) {
+    let first = [...d1];
+    let second = [...d2];
+    if (first.length === 0) {
+      this.setState({ currentCard: "The Deck is Empty Reshuffling" });
+      this.setState({ deck: d2 });
+      this.setState({ discard: [] });
+    } else {
+      this.setState({ currentCard: first[0] });
+      second.push(first[0]);
+      first.splice(0, 1);
+      this.setState({ deck: first });
+      this.setState({ discard: second });
+    }
+  }
+
+  componentDidMount() {
+    this.shuffleArray(this.state.deck);
+  }
+
+  render() {
+    return (
+      <div class="d-flex justify-content-center">
+        <Button
+          color="primary"
+          size="large"
+          onClick={() => this.handShuffle(this.state.deck)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {"Shuffle Deck"}
+        </Button>
+        <Button
+          color="success"
+          size="large"
+          onClick={() => this.pickACard(this.state.deck, this.state.discard)}
+        >
+          {"Pick a Card"}
+        </Button>
+        <h1> Your Card : {this.state.currentCard}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
